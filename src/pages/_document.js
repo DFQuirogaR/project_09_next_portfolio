eb8693e75ff7b8a75ab75835cbaf8e6921a69f48
Diego Quiroga <113153053+DFQuirogaR@users.noreply.github.com>
2024-08-09 00:51:14 -0500

    NProgress Completed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Nprogress css */}
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
